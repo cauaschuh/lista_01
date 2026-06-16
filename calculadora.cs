19bee0490d40243aacb5d81486adb360a9767e7b
caua <caua_schuh@estudante.sesisenai.org.br>
2026-06-15 22:21:40 -0300

    calculadora

---
+++ calculadora.cs
@@ -0,0 +1,21 @@
+public class Calculadora
+{
+    public int Somar(int a, int b)
+    {
+    }
+    
+    
+    public int Multiplicar(int a, int b)
+    {
+}
+
+public double Delta(double a, double b, doublec)
+    {
+        return Math.Pow(b, 2.0) - (4.0 * a *c);
+    }
+public double Baskharax1(double a,double b, double delta)
+    {
+        return ((b * -1.0) + Math.squrt(delta)) / (2.0 * a);
+    }
+
+}
